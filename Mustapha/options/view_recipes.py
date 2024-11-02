d2acc996f2fb337062ba8b91fc71202783ceb72d
OuafidHabchi <habchi.ouafid@gmail.com>
2024-11-02 14:26:08 -0400

    Update view_recipes.py

--- Mustapha/options/view_recipes.py
+++ Mustapha/options/view_recipes.py
@@ -8,16 +8,15 @@ import os
 client = MongoClient("mongodb+srv://wafid:wafid@ouafid.aihn5iq.mongodb.net")
 db = client["mustapha"]
 collection = db["create"]
-
-# Charger et afficher le logo avec un chemin absolu
-logo_path = os.path.abspath("options/images/image.png")
-try:
-    logo = Image.open(logo_path)
-    st.sidebar.image(logo, use_column_width=True)
-except FileNotFoundError:
+def view_recipes_page():
+    # Charger et afficher le logo avec un chemin absolu
+    logo_path = os.path.abspath("options/images/image.png")
+    try:
+        logo = Image.open(logo_path)
+        st.sidebar.image(logo, use_column_width=True)
+    except FileNotFoundError:
     st.sidebar.error("Logo image not found. Please check the file path.")
 
-def view_recipes_page():
     st.title("💊 Pharmaceutical Recipe Viewer")
 
     # Récupérer toutes les recettes de la collection MongoDB
