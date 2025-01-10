8b163030c3e2d2895d514efdd4a418b2b4fae8b9
OuafidHabchi <habchi.ouafid@gmail.com>
2025-01-10 11:55:40 -0500

    Update page_4.py

--- Mustapha/page_4.py
+++ Mustapha/page_4.py
@@ -4,7 +4,7 @@ from io import BytesIO
 import os
 import pandas as pd
 
-def create_pdf_without_password(product_info, steps, prepared_by, ui_table_data
+def create_pdf_without_password(product_info, steps, prepared_by, ui_table_data) :
     """Generate a professionally designed PDF with product info, a Equipements table, and detailed steps."""
     from fpdf import FPDF
     from io import BytesIO
