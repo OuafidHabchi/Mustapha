e1a178c6e7a2fdd8af5b53716a78a4294b4feef3
OuafidHabchi <habchi.ouafid@gmail.com>
2025-01-27 09:32:13 -0500

    Update page_4.py

--- Mustapha/page_4.py
+++ Mustapha/page_4.py
@@ -2,7 +2,7 @@ import streamlit as st
 from fpdf import FPDF
 from io import BytesIO
 import os
-import pandas as p
+import pandas as pd
 
 def create_pdf_without_password(product_info, steps, prepared_by, ui_table_data):
     """Generate a professionally designed PDF with product info, a Equipements table, and detailed steps."""
